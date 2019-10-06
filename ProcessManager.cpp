7913b1d26a99f8081f148bed20585be17d2cb7b4
Josh de Kock <josh@itanimul.li>
2019-10-06 18:31:01 +0100

    Another emit example

--- ProcessManager.cpp
+++ ProcessManager.cpp
@@ -1,23 +1,38 @@
 #include "ProcessManager.h"
 
 #include <QDebug>
+#include <QEventLoop>
 
 ProcessManager::ProcessManager(QObject *parent):
     QObject(parent)
 {
     Q_ASSERT(connect(&mProcess, QOverload<int, QProcess::ExitStatus>::of(&QProcess::finished), this, &ProcessManager::finished));
-    qDebug() << "Connected successfully";
     // Doesn't work
     // emit mProcess.finished(0, QProcess::NormalExit);
 }
 
 void ProcessManager::run()
 {
-    if (!mCommand.isEmpty())
-        mProcess.execute(mCommand);
+    if (mCommand.isEmpty())
+        return;
+
+    mProcess.start(mCommand);
+    if (!mProcess.waitForStarted(1000)) {
+        qDebug() << "Starting process failed or timed out (1000ms)";
+        return;
+    }
 
     // Works
 #if 1
+    // Since QML is calling from a separate thread without an
+    // event loop we need to add our own.
+    QEventLoop loop;
+    connect(&mProcess, QOverload<int>::of(&QProcess::finished), &loop, &QEventLoop::quit);
+    loop.exec();
+    emit finished(0, QProcess::NormalExit);
+#endif
+    // Works
+#if 0
     mProcess.waitForFinished(-1);
     qDebug() << "Wait for finshed";
     emit finished(0, QProcess::NormalExit);
